cf60debfe4e4c8320650570cf3b54cde63ef2094
Fer Martin <fernandoMartinSantana@gmail.com>
2020-02-24 12:17:10 +0000

    lebron jumping, enemies WIP

--- script.js
+++ script.js
@@ -1,23 +1,48 @@
-//Logic Funcions
-
-var quiet = false;
-
-document.addEventListener('KeyboardEvent'.KeyUp, function heroJump() { }, true);
-
-
-
-
-function heroJump() {
-  var elem = document.getElementById('hero');
-  var pos = 10; //Valor de la posicion inicial
-  // var initialPos = setInterval(animation, 0); // Velocidad mov. en milisegundos
-  function animation() {
-    if (pos == 240) {
-      clearInterval(initialPos);
+let hero = {
+  html: document.getElementById('hero'),
+  pos: 100,
+  posJump: 300,
+  goingUp: true,
+  animated: null,
+  move: function () {
+    // Detect TOP position
+    if (this.pos == this.posJump) {
+      this.goingUp = false;
+    }
+    // Detect END of movement
+    if (this.pos == 100 && !this.goingUp) {
+      this.goingUp = true;
+      clearInterval(this.animated);
+      this.animated = null;
+    }
+    // Calculate new position
+    if (this.goingUp) {
+      this.pos += 5;
     } else {
-      pos--;
-      elem.style.top = pos + "px";
+      this.pos -= 5;
+    }
+    // Update HTML
+    this.html.style.bottom = `${this.pos}px`
+  },
+  jump: function () {
+    if (this.animated == null) {
+      this.animated = setInterval(this.move.bind(this), 10);
     }
   }
 }
-heroJump();
+
+document.addEventListener("keydown", function (event) {
+  if (event.code === "Space" || event.code === "ArrowUp") {
+    hero.jump();
+  }
+});
+
+function Enemies(id, src, alt) {
+  this.id = id;
+  this.src = src;
+  this.alt = alt;
+
+}
+
+const hand = new Enemy("hand", "/images/zombie-hand.png", "mano zombie")
+const zombie = new Enemy("zombie", "/images/zombie.png", "zombie")
